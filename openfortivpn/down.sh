256cfcde56935b62fef6aafff7347847c751d29f
kunniva <dandujar@asdf-lkj>
2019-03-09 03:16:11 +0100

    Salto linea

--- openfortivpn/down.sh
+++ openfortivpn/down.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Comprobamos si existe un proceso ya existente y optenemos el pid
+#Comprobamos si existe un proceso ya existente y optenemos el pid.
 var=$(ps aux | grep "sudo openfortigui" | grep -v grep | awk '{print $2}')
 
 #Si existe matamos proceso y pedimos contraseña sudo. Y si no existe matamos todos los procesos de openfortigui.
@@ -10,4 +10,5 @@ else
 	killall openfortigui
 fi
 
-#Los procesos con "sudo openfortigui" son los que se ejecutan desde la barra por consola.#Los proceos de programa openfortigui son los ejecutados desde la interface gráfica del programa.
+#Los procesos con "sudo openfortigui" son los que se ejecutan desde la barra por consola.
+#Los proceos de programa openfortigui son los ejecutados desde la interface gráfica del programa.
