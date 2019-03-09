1bd0526b8bcff215eca95c71c17d2c3d1ff71959
kunniva <dandujar@asdf-lkj>
2019-03-09 03:24:11 +0100

    Modificar variable

--- openfortivpn/up.sh
+++ openfortivpn/up.sh
@@ -2,9 +2,9 @@
 
 case $1 in
 	HOLA)
-		sudo openfortigui --start-vpn --vpn-name HOLA &
+		sudo openfortigui --start-vpn --vpn-name name_profile_vpn &
 		;;
 	HOLAcom)
-		sudo openfortigui --start-vpn --vpn-name HOLAcom &
+		sudo openfortigui --start-vpn --vpn-name name_profile_vpn2 &
 		;;
 esac
